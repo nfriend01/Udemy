32ef41ec6cc10cafe4bdf4c8e2453945905d15da
Nick Friend <nicholasfriend91@gmail.com>
2018-08-08 14:50:23 -0400

    First Save

---
+++ Javascript Exercise/facebookScript.js
@@ -0,0 +1,53 @@
+var database = [
+	{
+		username: "nick",
+		password: "supersecret"
+	},
+	{
+		username: "sally",
+		password: "123"
+	},
+	{
+		username: "ingrid",
+		password: "777"
+	}
+];
+
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+
+	{
+		username: "Sally",
+		timeline: "Javascript is soooo cooool!"
+	},
+
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeety cool!"
+	}
+];
+
+function isUserValid(username, password) {
+	for (var i = 0; i < database.length; i ++) {
+		if (database[i].username === username && database[i].password === password) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function signIn(username, password) {
+	if (isUserValid(username, password)) {
+		console.log(newsfeed);
+	} else {
+		alert("Sorry, wrong username and password!");
+	}
+}
+
+var userNamePrompt = prompt("What\'s your username?");
+var passwordPropmt = prompt("What\'s your password?");	
+
+signIn(userNamePrompt, passwordPropmt);
